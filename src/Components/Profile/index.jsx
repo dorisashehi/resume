1f6d01387a53ba3734088e5a6ba7e09cb84bd02b
Dorisa Shehi <doris.shehi2@gmail.com>
2024-06-16 10:28:27 +0200

    Profile empty value added when link removed

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -10,7 +10,7 @@ function Profile (props) {
         setEdit(!edit)
     }
 
-    const [profile, setProfile] = useState({
+    const [profile, setProfile] = useState({ //PROFILE STATE DEFAULT VALUE EMPTY
         fullName: '',
         email: '',
         phone: '',
@@ -65,10 +65,28 @@ function Profile (props) {
         setOpenLinkedin(!linkedIn);
     }
 
+    const setEmptyValue = (field) => { //ADD EMPTY VALUE FOR OTHER LINKS
+        return {
+            ...profile,
+            others: {
+                ...profile.others,
+                [field]: ''
+            }
+        }
+    }
+
     const removeField = (field) => { //REMOVE FORM FIELDS FOR LINKS ON CLICK REMOVE ICON
+
         if(field === "website") openWeb()
         if(field === "github") openGithub()
         if(field === "linkedin") openLinkedIn()
+
+        //UPDATE THE PROFILE OBJECT
+        setProfile(setEmptyValue(field))
+
+        //UPDATE THE PARENT OBJECT PROFILE
+        props.addProfile(setEmptyValue(field));
+
     }
 
 
@@ -81,21 +99,21 @@ function Profile (props) {
 
                 </div>
                 <div className="name">Your name</div>
-                <div className="position">
-                    { profile.location }
-                </div>
+                {/* <div className="position">
+                    { (profile.location !== '') ? profile.location : 'Adress' }
+                </div> */}
                 <ul className="personal-info">
                     <li className="item">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-                        { profile.email }
+                        { (profile.email !== '') ? profile.email : 'Email' }
                     </li>
                     <li className="item">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-phone-call"><path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                        { profile.phone }
+                        { (profile.phone !== '') ? profile.phone : 'Phone' }
                     </li>
                     <li className="item">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-                        { profile.location }
+                        { (profile.location !== '') ? profile.location : 'Address'}
                     </li>
                 </ul>
             </div>
